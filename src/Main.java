6f1dd768bea1d451c766a1061850d40a5693ed09
silfo <a143050@gmail.com>
2017-09-30 14:21:59 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n;
+        n=in.nextInt();
+        System.out.printf("%d\n",n*9/5+32);
+    }
+}
